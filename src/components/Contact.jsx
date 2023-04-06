7396e7d5281eb7ccb4d9107fecf17c0348d4b196
Tommy Zumstien <zumstein@seattleu.edu>
2023-04-06 12:27:41 -0700

    fixed contact width on desktop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='bg-black-100 p-8 rounded-2xl align-middle'
+        className='flex-[1] bg-black-100 p-8 rounded-2xl align-middle'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
