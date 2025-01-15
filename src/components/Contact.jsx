6c5e8559c6e40cb71928f0e3902983bcc44dd1ab
Tommy Zumstien <zumstein@seattleu.edu>
2025-01-15 15:52:23 -0800

    idk but there were changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,6 @@ const Contact = () => {
         variants={slideIn("left", "tween", 0.2, 1)}
         className='flex-[1] bg-black-100 p-8 rounded-2xl align-middle'
       >
-        <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
         <div className="mt-6 flex gap-6">
           <button 
